3845f027ab4c6d4feea6c1e9e6baeee0b11b3dfd
Om Mule <dgr8om@gmail.com>
2021-05-27 13:25:39 +0530

    Fx java files

---
+++ JavaFx-calculator/Model.java
@@ -0,0 +1,23 @@
+package application;
+
+
+public class Model {
+	public float calculate(long num1, long num2, String operator) {
+		switch (operator) {
+		case "+":
+			return num1+num2;
+		case "-":
+			return num1-num2;
+		case "*":
+			return num1*num2;
+		case "/":
+			if (num2 == 0) {
+				return 0;
+			}
+			return num1/num2;
+
+		default:
+			return 0;
+		}
+	}
+}
